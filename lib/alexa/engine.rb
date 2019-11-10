e4844a04ceaf133aba5861c5d17960b280249883
Carsten Reinke <creinke@berlinbag.com>
2019-11-10 20:53:08 +0100

    Enable decorators support

--- lib/alexa/engine.rb
+++ lib/alexa/engine.rb
@@ -1,5 +1,11 @@
 module Alexa
   class Engine < ::Rails::Engine
     isolate_namespace Alexa
+
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
